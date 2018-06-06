5d4840e62486e423566ffd09850425d7ef84379f
snowchenlei <gbdvjxu1257q@outlook.com>
2018-06-06 21:15:23 +0800

    添加日志实体文件

---
+++ src/Framework/Cl.AuthorityManagement.Entity/Log/LoginInfo.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cl.AuthorityManagement.Entity
+{
+    public class LoginInfo : Entity
+    {
+
+        [StringLength(50)]
+        public string IP { get; set; }
+
+        public string Device { get; set; }
+        public string OS { get; set; }
+        public string UserAgent { get; set; }
+
+        [JsonIgnore]
+        public UserInfo UserInfo { get; set; }
+    }
+}
---
+++ src/Framework/Cl.AuthorityManagement.Entity/Log/OperateInfo.cs
@@ -0,0 +1,20 @@
+﻿using Cl.AuthorityManagement.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cl.AuthorityManagement.Entity
+{
+    public class OperateInfo: Entity
+    {
+        public OperateType OperateType { get; set; }
+
+        [StringLength(500)]
+        public string Remark { get; set; }
+
+        public UserInfo CreateUser { get; set; }
+    }
+}
